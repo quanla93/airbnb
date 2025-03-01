868e688d5d5cb2deef05c3f9a269319236919d60
quanla <quanla.work@gmail.com>
2025-03-01 15:45:21 +0700

    [CREATE] Create module share common

---
+++ airbnb/airbnb/src/main/java/com/airbnb/shared/domain/AbstractAuditingEntity.java
@@ -0,0 +1,32 @@
+package com.airbnb.shared.domain;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractAuditingEntity implements Serializable {
+
+    public abstract T getId();
+
+    @CreatedDate
+    @Column(updatable = false, name = "created_date")
+    private Instant createdDate = Instant.now();
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date")
+    private Instant lastModifiedDate = Instant.now();
+
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Instant getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Instant lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
