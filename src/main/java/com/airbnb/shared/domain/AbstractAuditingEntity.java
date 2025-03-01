1fc3820977901e0c1e8e52df035f7b570cd8e329
quanla <quanla.work@gmail.com>
2025-03-01 15:53:21 +0700

    [REFACTOR] Refactor module share common
    Refer issue #4 [https://github.com/quanla93/airbnb/issues/4]

--- src/main/java/com/airbnb/shared/domain/AbstractAuditingEntity.java
+++ src/main/java/com/airbnb/shared/domain/AbstractAuditingEntity.java
@@ -1,8 +1,18 @@
 package com.airbnb.shared.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.Instant;
+
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class AbstractAuditingEntity implements Serializable {
+public abstract class AbstractAuditingEntity<T> implements Serializable {
 
     public abstract T getId();
 
